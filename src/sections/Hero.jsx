bdb13c7b1abdd3e026622ef484c80326acf6150c
Natoli Tesgera <fs.natoli@gmail.com>
2025-03-01 21:19:33 -0600

    Corrected typo

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <p className="sm:text-4xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Natoli <span className="waving-hand">👋</span>
         </p>
-        <p className=" hero_tag text-gray_gradient  z-10 mt-60 ">Aspring software and web devloper</p>
+        <p className=" hero_tag text-gray_gradient  z-10 mt-60 ">Aspiring software and web devloper</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
